1aae6a7e6933b9abcc58336ea71f39d7fed3f9b2
Allen Wu <allenwu1018@gmail.com>
2014-06-02 17:53:32 +0800

    serve static files

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-var server = require("../index");
+var miniHarp = require("../index");
 var argv = require('minimist')(process.argv.slice(2));
 
 var port = argv.port || 4000;
+var root = argv._[0];
+
+var app = miniHarp(root || process.cwd());
+
 
 console.log("Starting mini-harp on http://localhost:" + port);
-server.listen(port);
+app.listen(port);
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var connect = require('connect');
+var serveStatic = require('serve-static');
 
 // create app
-var miniHarp = connect();
+var miniHarp = function(root) {
+  root = root || process.cwd();
+  return connect().use(serveStatic(root));
+};
+
 
 module.exports = miniHarp;
