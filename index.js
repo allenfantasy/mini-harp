c088f0106c9bb9d03c0710c15e7c07acffcbfa14
Allen Wu <allenwu1018@gmail.com>
2014-06-03 10:53:26 +0800

    preprocessor of jade

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade')
 
 // create app
 var miniHarp = function(root) {
   root = root || process.cwd();
-  return connect().use(serveStatic(root));
+  //return connect().use(serveStatic(root));
+  return connect().use(makeJade(root));
 };
 
 
