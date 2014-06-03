8ba90d738d2a46a47faf2c13277745ac2e6c92aa
Allen Wu <allenwu1018@gmail.com>
2014-06-03 13:15:01 +0800

    add middleware that rewrites "request.url"

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade')
 var makeLess = require('./lib/processor/less')
+var dispatch = require('./lib/middleware/dispatch')
 
 // create app
 var miniHarp = function(root) {
   root = root || process.cwd();
   //return connect().use(serveStatic(root));
-  return connect().use(makeJade(root)).use(makeLess(root));
+  return connect().use(dispatch()).use(makeJade(root)).use(makeLess(root));
 };
 
 
