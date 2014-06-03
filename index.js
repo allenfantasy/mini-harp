f7ab9bd405c4b3c63da8196da913c796b9dbf010
Allen Wu <allenwu1018@gmail.com>
2014-06-03 12:54:37 +0800

    less

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade')
+var makeLess = require('./lib/processor/less')
 
 // create app
 var miniHarp = function(root) {
   root = root || process.cwd();
   //return connect().use(serveStatic(root));
-  return connect().use(makeJade(root));
+  return connect().use(makeJade(root)).use(makeLess(root));
 };
 
 
